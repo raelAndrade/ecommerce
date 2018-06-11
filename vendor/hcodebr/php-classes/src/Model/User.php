95de05d01c32cde58e4f0190edd0f2db198caf29
raelAndrade <israelg.andrade@bol.com.br>
2018-06-11 13:26:40 -0300

    Site login

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,6 +10,8 @@ class User extends Model{
 
   const SESSION = "User";
   const FORGOT_SECRET = "HcodePHP7_Secret";
+  const ERROR = "UserError";
+  const ERROR_REGISTER = "UserErrorRegister";
 
   public static function getFromSession()
   {
@@ -23,21 +25,22 @@ class User extends Model{
   public static function checkLogin($inadmin = true)
   {
     if(
-       !isset($_SESSION[User::SESSION])
+       !isset($_SESSION[User::SESSION]) // se a sessão do usuario não for definida, quer dizer que ele não está logado
        ||
-       !$_SESSION[User::SESSION]
+       !$_SESSION[User::SESSION] // se a sessão estiver vazia, tb não está logado
        ||
-       !(int)$_SESSION[User::SESSION]["iduser"] > 0
+       !(int)$_SESSION[User::SESSION]["iduser"] > 0 // se a sessão está definida e não for vazia, mas o iduser não é maior que 0, significa que tb não está logado
      ) {
        // Não está logado
-       return false;
-     } else {
+       return false; // então retorna false
+     } else { // senão
        if ($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'] === true) {
-         return true;
-       } else if ($inadmin === false) {
-         return true;
-       } else {
-         return false;
+         // se o inadmin é igual a true e o boolean da sessão do inadim for igual a true
+         return true; // retorna true
+       } else if ($inadmin === false) { // se essa rota não é da administração for igual a false
+         return true; // retorna a true
+       } else { // se algo for diferente de disso, logo não está logado
+         return false; // pede para o usuario fazer a autenticação
        }
      }
 
@@ -45,7 +48,7 @@ class User extends Model{
 
   public static function login( $login, $password ){
        $sql = new Sql();
-       $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+       $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
        ":LOGIN" => $login
        ) );
        if (count($results) === 0){
@@ -54,6 +57,7 @@ class User extends Model{
        $data = $results[0];
        if ( password_verify( $password, $data[ "despassword" ] ) === true ){
            $user = new User();
+           $data['desperson'] = utf8_encode($data['desperson']);
            $user->setData( $data );
            $_SESSION[ User::SESSION ] = $user->getValues();
            return $user;
@@ -65,7 +69,11 @@ class User extends Model{
    public static function verifyLogin($inadmin = true)
    {
      if (!User::checkLogin($inadmin)) {
-       header( "Location: /admin/login" );
+       if ($inadmin) {
+         header( "Location: /admin/login" );
+       } else {
+          header( "Location: /login" );
+       }
        exit;
     }
   }
@@ -87,7 +95,7 @@ class User extends Model{
       $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
         ":desperson"=>utf8_decode($this->getdesperson()),
         ":deslogin"=>$this->getdeslogin(),
-        ":despassword"=>$this->getdespassword(),
+        ":despassword"=>User::getPasswordHash($this->getdespassword()),
         ":desemail"=>$this->getdesemail(),
         ":nrphone"=>$this->getnrphone(),
         ":inadmin"=>$this->getinadmin()
@@ -103,6 +111,7 @@ class User extends Model{
     ));
 
     $data = $results[0];
+    $data['desperson'] = utf8_encode($data['desperson']);
     $this->setData($data);
   }
 
@@ -111,9 +120,9 @@ class User extends Model{
     $sql = new Sql();
     $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
       ":iduser"=>$this->getiduser(),
-      ":desperson"=>$this->getdesperson(),
+      ":desperson"=>utf8_decode($this->getdesperson()),
       ":deslogin"=>$this->getdeslogin(),
-      ":despassword"=>$this->getdespassword(),
+      ":despassword"=>User:getPasswordHash($this->getdespassword()),
       ":desemail"=>$this->getdesemail(),
       ":nrphone"=>$this->getnrphone(),
       ":inadmin"=>$this->getinadmin()
@@ -206,6 +215,45 @@ class User extends Model{
        ":iduser"=>$this->getiduser()
     ));
   }
+
+  public static function setError($msg)
+  {
+    $_SESSIO[User::ERROR] = $msg;
+  }
+
+  public static function getError()
+  {
+    $msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+    User::clearError();
+    return $msg;
+  }
+
+  public static function clearError()
+  {
+    $_SESSION[User::ERROR] = NULL;
+  }
+
+  public static function setErrorRegister()
+  {
+    $_SESSION[User::ERROR_REGISTER] = $msg;
+  }
+
+  public static function checkLoginExist($login)
+  {
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+      ':deslogin'=>$login
+    ]);
+    return (count($results) > 0);
+  }
+
+  public static function getPasswordHash($password)
+  {
+    return password_hash($password, PASSWORD_DEFAULT, [
+      'cost'=>12
+    ]);
+  }
+
 }
 
 ?>
